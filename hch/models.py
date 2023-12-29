f7e3d9458d6511e67a5c8714e658fb5d9431cdde
jaaz7 <jaazielvale@hotmail.com>
2023-12-29 23:09:59 +0100

    Delete all files

--- hch/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
-
-
-
-# Create your models here.
-
--- hch/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.contrib import messages
-
-
-# Create your views here.
-def home(request):
-    return render(request, 'home_cooked_harmony/index.html')
