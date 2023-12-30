009a924c163ca431cd6dfa29184b1c8dbf89cc5b
jaaz7 <jaazielvale@hotmail.com>
2023-12-30 22:39:11 +0100

    Configure add_post function on views.py to validate form

--- home_cooked_harmony/views.py
+++ home_cooked_harmony/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Post
+from .forms import PostForm
 
 
 class PostList(generic.ListView):
@@ -20,5 +21,12 @@ def post_detail(request, slug):
 
 
 def add_post(request):
-    model = Post
-    return render(request, "add_post.html")
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("home")
+    return render(request, "add_post.html", {"form": form})
