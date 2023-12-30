f33e49b51059976f7c7948a0268e4fe7810bf9c0
jaaz7 <jaazielvale@hotmail.com>
2023-12-30 18:35:10 +0100

    Add prepopulated fields to slug with the content of title so it appears in the admin page

--- home_cooked_harmony/admin.py
+++ home_cooked_harmony/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
