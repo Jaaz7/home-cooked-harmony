5b56afd8c28b62129053edbfdb9b0c954d7c3a66
jaaz7 <jaazielvale@hotmail.com>
2023-12-30 18:20:13 +0100

    Add slug to Post model

--- home_cooked_harmony/models.py
+++ home_cooked_harmony/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
