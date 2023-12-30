def7affa628963ba6810a03f03f7d95b0ddd8887
jaaz7 <jaazielvale@hotmail.com>
2023-12-30 03:07:26 +0100

    Modify text in header and footer in base.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,6 @@
-/*!
-* Start Bootstrap - Blog Post v5.0.9 (https://startbootstrap.com/template/blog-post)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(document).ready(function () {
+  var currentYear = new Date().getFullYear();
+  $("#copyright").text(function (i, origText) {
+    return origText + " " + currentYear;
+  });
+});
