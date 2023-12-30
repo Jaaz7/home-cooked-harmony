f70ffd1db9e46119e7dccbc4f39eb2e5adc1b48c
jaaz7 <jaazielvale@hotmail.com>
2023-12-30 14:38:41 +0100

    Render posts in the main page, fix sticky footer

--- home_cooked_harmony/views.py
+++ home_cooked_harmony/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
 
 # Create your views here.
-def home(request):
-    return render(request, "index.html")
+# def home(request):
+#     return render(request, "index.html")
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all().order_by("-date")
+    template_name = "index.html"
+    paginate_by = 3
