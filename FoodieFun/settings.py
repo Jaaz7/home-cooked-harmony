feb38ea43114668335b9112523d4add943436273
jaaz7 <jaazielvale@hotmail.com>
2024-01-05 20:09:55 +0100

    Add minimum password length validator

--- FoodieFun/settings.py
+++ FoodieFun/settings.py
@@ -107,6 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 9,
+        },
+    },
 ]
 
 
