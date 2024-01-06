8be5c33aeb8b2ac26e3954a9835b296575354554
jaaz7 <jaazielvale@hotmail.com>
2024-01-06 20:35:51 +0100

    Add comments

--- home_cooked_harmony/forms.py
+++ home_cooked_harmony/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import Post, Comment
 
-
+# Form class for creating or editing a Post
 class PostForm(forms.ModelForm):
+    # Making the image field optional
     image = forms.ImageField(required=False)
 
     class Meta:
+        # The model that the form will interact with
         model = Post
+        # Fields from the Post model to include in the form
         fields = [
             "title",
             "description",
@@ -15,8 +18,10 @@ class PostForm(forms.ModelForm):
             "servings",
         ]
 
-
+# Form class for creating a Comment
 class CommentForm(forms.ModelForm):
     class Meta:
+        # The model that the form will interact with
         model = Comment
+        # Field from the Comment model to include in the form
         fields = ["body"]
